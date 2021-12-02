3f14cd55bf2bf1c56346f31486c42c47846b2afc
MEGABOT-HACK <92226109+MEGABOT-HACK@users.noreply.github.com>
2021-12-02 10:27:21 -0600

    Update install-papaviruz.sh

--- install-papaviruz.sh
+++ install-papaviruz.sh
@@ -1,8 +1,9 @@
+cd
+termux-setup-storage
 pkg upgrade && pkg update -y
 pkg install python
 pkg install pv
 git clone https://github.com/Hacking-pch/papaviruz
 cd papaviruz
 chmod +x *
-rm install-papaviruz
 bash papaviruz.sh
